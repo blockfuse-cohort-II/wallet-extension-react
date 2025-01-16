9d49d1b5e7fb53b04b0d484623fdb4e2ba34f9d4
Kudaru Deborah <deborahkudaru@gmail.com>
2025-01-16 11:15:26 -0800

    feat: home and create-wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import CreateWallet from "./pages/onboarding/create-wallet";
+import CreatePassword from "./pages/onboarding/create-password";
 
 function App() {
   return (
     <div className="h-full w-full">
       <Routes>
-        <Route path="/" element={<CreateWallet />} />
+        <Route path="/" element={<CreatePassword />} />
+        <Route path="/create-wallet" element={<CreateWallet />} />
         {/* <Route path="*" element={<NotFound />} />  */}
       </Routes>
     </div>
--- src/pages/onboarding/create-password/index.tsx
+++ src/pages/onboarding/create-password/index.tsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom";
+
 const CreatePassword = () => {
   return (
-    <div>CreatePassword</div>
+    <div className="p-10">
+        {/* <Navbar /> */}
+        <div className="text-center pt-20 ">
+          <h1 className="text-gray-800 font-bold text-3xl">Wallet</h1>
+          <p className="pt-10">Welcome to your Web3 Wallet made just for you.</p>
+          <div className="pt-20 flex flex-col gap-8">
+            <div>
+              <Link to="/create-wallet" className=" bg-[#FEC84B]  text-white px-4 py-2 rounded font-bold">Create a Wallet</Link>
+            </div>
+            <div>
+            <Link to="/" className="bg-slate-800 text-white px-4 py-2 rounded font-bold">Sign in with seed phrase</Link>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 };
 
