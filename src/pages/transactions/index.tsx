6f1fe30bd4b93d81b02b1159b7f5c31e701e069e
Emeka web dev <emekarexchukwu@gmail.com>
2025-01-20 00:30:32 +0100

    modify: transaction input

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -4,47 +4,53 @@ import Minus from "../../components/icons/Minus";
 import Plus from "../../components/icons/Plus";
 import Search from "../../components/icons/Search";
 
-
 const Transactions = () => {
-    return (
-      <div className="text-white/50 p-10">
-          <div className="flex gap-2 bg-white/5 px-4 py-2 rounded-lg"><Search /><div></div><input type="search" placeholder="Search..." className="text-white/50 bg-transparent outline-0 border-0"/></div>
-          <div>
-            <h3 className="py-3">November</h3>
-  
-            <div className="flex flex-col gap-3">
-              <div className=" bg-white/5 flex gap-2 px-5 py-2 rounded-lg">
-                <Plus />
-                <div className="text-sm">
-                  <p className="text-white">11/11/2024 23:05</p>
-                  <p>7.43 LINK</p>
-                </div>
-              </div>
-              <div className=" bg-white/5 flex gap-2 px-5 py-2 rounded-lg">
-              <Minus />
-                <div className="text-sm">
-                  <p className="text-white">11/11/2024 23:05</p>
-                  <p>7.43 ETH</p>
-                </div>
-              </div>
-              <div className=" bg-white/5 flex gap-2 px-5 py-2 rounded-lg">
-              <ArrowDown />
-                <div className="text-sm">
-                  <p className="text-white">11/11/2024 23:05</p>
-                  <p>7.43 ETH</p>
-                </div>
-              </div>
-              <div className=" bg-white/5 flex gap-2 px-5 py-2 rounded-lg">
-              <ArrowUp />
-                <div className="text-sm">
-                  <p className="text-white">11/11/2024 23:05</p>
-                  <p>7.43 ETH</p>
-                </div>
-              </div>
+  return (
+    <div className="text-white/50 p-10">
+      <div className="flex gap-2 bg-white/5 px-4 py-2 rounded-lg">
+        <Search />
+        <div></div>
+        <input
+          placeholder="Search..."
+          className="text-white/50 bg-transparent outline-0 border-0"
+        />
+      </div>
+      <div>
+        <h3 className="py-3">November</h3>
+
+        <div className="flex flex-col gap-3">
+          <div className=" bg-white/5 flex gap-2 px-5 py-2 rounded-lg">
+            <Plus />
+            <div className="text-sm">
+              <p className="text-white">11/11/2024 23:05</p>
+              <p>7.43 LINK</p>
             </div>
           </div>
+          <div className=" bg-white/5 flex gap-2 px-5 py-2 rounded-lg">
+            <Minus />
+            <div className="text-sm">
+              <p className="text-white">11/11/2024 23:05</p>
+              <p>7.43 ETH</p>
+            </div>
+          </div>
+          <div className=" bg-white/5 flex gap-2 px-5 py-2 rounded-lg">
+            <ArrowDown />
+            <div className="text-sm">
+              <p className="text-white">11/11/2024 23:05</p>
+              <p>7.43 ETH</p>
+            </div>
+          </div>
+          <div className=" bg-white/5 flex gap-2 px-5 py-2 rounded-lg">
+            <ArrowUp />
+            <div className="text-sm">
+              <p className="text-white">11/11/2024 23:05</p>
+              <p>7.43 ETH</p>
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
-  
-  export default Transactions;
+    </div>
+  );
+};
+
+export default Transactions;
